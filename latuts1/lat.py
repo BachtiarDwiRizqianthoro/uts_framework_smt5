85d948869750b887a560ed399bf03477bee824a0
Bachtiar Dwi Rizqianthoro <72340654+BachtiarDwiRizqianthoro@users.noreply.github.com>
2022-11-13 17:45:37 +0700

    Add files via upload

---
+++ latuts1/lat.py
@@ -0,0 +1,21 @@
+from flask import flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = flask (__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+db = SQLAlchemy(app)
+class students(db.Model):
+   id = db.Column('student_id', db.Integer, primary_key = True)
+   name = db.Column(db.String(100))
+   city = db.Column(db.String(50))
+   addr = db.Column(db.String(200))
+   pin = db.Column(db.String(10))
+
+def _init_(self, name, city, addr,pin):
+   self.name = name
+   self.city = city
+   self.addr = addr
+   self.pin = pin
+
+   db.create_all()
+
